af11498cc832af14cdbfff3d49f8c77353756eb7
juliomorais06 <julio.fariasmorais@hotmail.com>
2023-11-17 17:14:15 -0300

    change código recursividade

--- aulas/aula48/Recursividade.cs
+++ aulas/aula48/Recursividade.cs
@@ -1,20 +1,6 @@
 using System;
 
 class Calc{
-    public int soma(params int[]n){
-        int s = 0;
-        for(int i = 0; i < n.Length; i++){
-            s += n[i];
-        }
-        return s;
-    }
-    public double soma(params double[]n){
-        double s = 0;
-        for(int i = 0; i < n.Length; i++){
-            s += n[i];
-        }
-        return s;
-    }
     public int fat(int n){
         int res;
         if(n <= 1){
