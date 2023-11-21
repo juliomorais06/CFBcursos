5431be3ef6a69b1bfc87cfacb5e64e93240a545c
juliomorais06 <julio.fariasmorais@hotmail.com>
2023-11-21 12:04:48 -0300

    Tratamento de excessões parte 1

---
+++ aulas/aula52/ExcecoesP1.cs
@@ -0,0 +1,18 @@
+using System;
+
+class ExcecoesP1{
+    static void Main(){
+        int n1, n2, res;
+        n1 = n2 = res = 0;
+        n1 = 10;
+        n2 = 0;
+        
+        try{
+            res = n1 / n2;   
+            Console.WriteLine("{0}/{1} = {2}", n1, n2, res); 
+        }catch(Exception e){
+            Console.WriteLine("ERRO: {0}", e.Message);
+            Console.WriteLine("Ex: {0}", e.GetType());
+        }
+    }
+}
